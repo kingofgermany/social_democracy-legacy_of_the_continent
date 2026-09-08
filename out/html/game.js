f58a9fc1b4b1cefbed15b4cb0c691140c922bb7e
kingofgermany <makenomista@keemail.me>
2026-09-08 16:02:19 +0800

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -329,6 +329,7 @@ window._musicPlaylist = [
     { name: "Frühlingslied", src: "music/1928_1930/FruhlingsliedMendelssohn.mp3" },
     { name: "Die Gedanken Sind Frei", src: "music/1928_1930/die-gedanken-sind-frei.mp3" },
     { name: "Liebesfreud", src: "music/1928_1933/liebesfreud.mp3" },
+    { name: "Die Internationale", src: "music/1930_1933/die-internationale.ogg" },
     { name: "Sozialistenmarsch", src: "music/1928_1933/sozialistenmarsch.mp3" },
     { name: "Zwei kleine Italiener", src: "music/1928_1933/zwei-kleine-italiener(1).mp3" },
     { name: "Beiß nicht gleich in jeden Apfel", src: "music/1928_1933/beiss-nicht-gleich-in-jeden-apfel.mp3" }
